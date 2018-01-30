c44edb53ca3886cdb1b90a13d515cef4cc88c777
Thomas Scheuchzer <thomas.scheuchzer2@swisscom.com>
2018-01-30 16:24:19 +0100

    Fixes gh-67

--- spring-cloud-gateway-core/src/main/java/org/springframework/cloud/gateway/handler/predicate/HeaderRoutePredicateFactory.java
+++ spring-cloud-gateway-core/src/main/java/org/springframework/cloud/gateway/handler/predicate/HeaderRoutePredicateFactory.java
@@ -15,8 +15,7 @@
  *
  */
 
-package org.springframework.cloud.gateway.handler.predicate
-		;
+package org.springframework.cloud.gateway.handler.predicate;
 
 import java.util.Arrays;
 import java.util.List;
